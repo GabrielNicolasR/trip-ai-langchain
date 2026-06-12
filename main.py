b7754c958c6641e778fd3f35b6abb5cc5910ccd2
Gabriel Nicolas <gabrielrochasa@gmail.com>
2026-06-11 22:01:09 -0300

    feat: implement JsonOutputParser with Pydantic for structured output

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import Field, BaseModel
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("O motivo pelo qual é interessante visitar essa cidade")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 prompt_cidade = PromptTemplate(
     template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,
-    input_variables=["interesse"]
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
 
 modelo = ChatOpenAI(
@@ -20,7 +29,7 @@ modelo = ChatOpenAI(
     api_key=api_key,
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia = prompt_cidade | modelo | parseador
 
 resposta = cadeia.invoke(
     {
